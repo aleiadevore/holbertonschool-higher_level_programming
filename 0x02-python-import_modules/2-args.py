b946d3e66a9c5f178414d43873588eafb1da2806
Aleia DeVore <2496@holbertonschool.com>
2020-12-10 22:02:02 +0000

    Removed 'size'

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
 size = len(sys.argv) - 1
 
 if size == 1:
-    print("size: {} argument.".format(size))
+    print("{} argument.".format(size))
     print("1: {}".format(sys.argv[1]))
 else:
-    print("size: {} arguments.".format(size))
+    print("{} arguments.".format(size))
     for i in range(1, size + 1):
         print("{}: {}".format(i, sys.argv[i]))
