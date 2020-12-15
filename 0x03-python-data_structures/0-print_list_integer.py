8a4f72b28fc849c7af97a74510282e894405967a
Aleia DeVore <2496@holbertonschool.com>
2020-12-15 20:31:48 +0000

    Created function to print elements in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    if __name__ == "__main__":
+        from main import my_list
+    output = ["{}".format(elem) for elem in my_list]
+    size = len(output)
+    for i in range(0, size):
+        print("{}".format(output[i]))
