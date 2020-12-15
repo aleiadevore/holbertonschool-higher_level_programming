791f24830c01ca686228903aae5f486db940d9e4
Aleia DeVore <2496@holbertonschool.com>
2020-12-15 20:54:25 +0000

    Updated to if index is negative rather than value of index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,9 +6,7 @@ def element_at(my_list, idx):
 
     if idx > size - 1:
         return (None)
-
-    x = my_list[idx]
-
-    if x < 0:
+    if idx < 0:
         return (None)
+    x = my_list[idx]
     return (x)
