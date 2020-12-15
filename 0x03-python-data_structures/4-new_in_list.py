07699e8abd4fc23d8d335454aa0265a89add4e0c
Aleia DeVore <2496@holbertonschool.com>
2020-12-15 21:53:41 +0000

    Created function to make copy of list and replace element at index

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    size = len(new_list)
+
+    if idx < 0:
+        return (new_list)
+    if idx > size:
+        return (new_list)
+
+    new_list[idx] = element
+
+    return (new_list)
