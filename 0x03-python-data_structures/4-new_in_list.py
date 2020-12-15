9a146778c13fb0a81258f02f10568af7a64d73f9
Aleia DeVore <2496@holbertonschool.com>
2020-12-15 21:57:12 +0000

    Updated length

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,7 @@ def new_in_list(my_list, idx, element):
 
     if idx < 0:
         return (new_list)
-    if idx > size:
+    if idx > size - 1:
         return (my_list)
 
     new_list[idx] = element
