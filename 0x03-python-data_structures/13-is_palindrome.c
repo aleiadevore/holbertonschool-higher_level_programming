4aeebfffd6f431e9e288358051e299ba607cb69e
Aleia DeVore <2496@holbertonschool.com>
2020-12-16 04:02:56 +0000

    Began funtion to determine if linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - determines if linked list is palindrome
+ * @head: head of linked list
+ * Return: 0 if not, 1 if is palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return (1);
+	else
+		return (0);
+}
