ee3c45366f0ef5df04fa4176389e5c196fe461d7
Aleia DeVore <2496@holbertonschool.com>
2020-12-16 02:26:45 +0000

    Updated to using comprehension

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,4 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            if j < len(matrix[i]) - 1:
-                print("{}".format(matrix[i][j]), end=' ')
-            else:
-                print("{}".format(matrix[i][j]))
+    print('\n'.join([' '.join([str(cell) for cell in row]) for row in matrix]))
