ac1fb7cbc9c47919931ada65c728beb946ad1adb
Aleia DeVore <2496@holbertonschool.com>
2020-12-18 01:40:41 +0000

    Created function to convert roman numerals to ints

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    sum = 0
+    prev = None
+    for i in reversed(roman_string):
+        if i == 'I':
+            if prev is None or prev is 'I':
+                sum += 1
+            else:
+                sum -= 1
+        if i == 'V':
+            if prev is None or prev is 'I' or prev is 'V':
+                sum += 5
+            else:
+                sum -= 5
+        if i == 'X':
+            if prev is None or prev is 'I' or prev is 'V' or prev is 'X':
+                sum += 10
+            else:
+                sum -= 10
+        if i == 'L':
+            if (prev is None or prev is 'I' or prev is 'V' or prev is 'X' or
+               prev is 'L'):
+                sum += 50
+            else:
+                sum -= 50
+        if i == 'C':
+            if prev is None or sum < 500:
+                sum += 100
+            else:
+                sum -= 100
+        if i == 'D':
+            if prev is None or sum < 1000:
+                sum += 500
+            else:
+                sum -= 500
+        if i == 'M':
+            if prev is None or sum < 1000:
+                sum += 1000
+            else:
+                sum -= 1000
+        prev = i
+    return sum
