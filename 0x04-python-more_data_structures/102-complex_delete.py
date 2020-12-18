73787338396d5bda8d73c86188fdb7fe73cfa85a
Aleia DeVore <2496@holbertonschool.com>
2020-12-18 03:03:47 +0000

    Returned first dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,8 +3,7 @@
 
 def complex_delete(a_dictionary, value):
     new = a_dictionary.copy()
-    for i in a_dictionary:
-        if a_dictionary[i] is value:
-            del new[i]
-    a_dictionary = new.copy()
+    for i in new:
+        if new[i] is value:
+            del a_dictionary[i]
     return a_dictionary
