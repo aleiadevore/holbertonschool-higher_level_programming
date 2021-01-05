681589fa2fa4a17d903159188e5557815e280394
Aleia DeVore <2496@holbertonschool.com>
2021-01-05 22:06:52 +0000

    Updated error handling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,8 @@
 class Square:
     """This class creates a square and tests for failures"""
     def __init__(self, size=0):
-        try:
-            type(size) is int
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except TypeError:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        except ValueError:
+        if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
