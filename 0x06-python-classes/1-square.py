68039c1880ef6aab218cfab3c2faee37844310af
Aleia DeVore <2496@holbertonschool.com>
2021-01-05 22:02:56 +0000

    Fixed documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""This module contains a class that initializes a single
+attribute
+"""
 
 
 class Square:
+    """This class creates a square and initializes a single attribute"""
     def __init__(self, size):
         self.__size = size
