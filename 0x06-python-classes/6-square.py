34e6bdf3814c3b626b219ad2611fdb7617052f95
Aleia DeVore <2496@holbertonschool.com>
2021-01-06 00:53:02 +0000

    Added fails to both initializer and setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""This module creates a class to create a square and print it"""
+"""This module creates a class to create a square and print it with a
+given position"""
 
 
 class Square:
     """This class creates a square, tests for failures, and then prints out
-    the square"""
+    the square with a given position"""
     def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        if type(position) is not tuple:
+        if position[0] is None or position[1] is None:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) is not int or type(position[1]) is not int:
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
@@ -23,10 +24,6 @@ class Square:
     # This gets the size
     def size(self):
         return(self.__size)
-
-    # This gets the position
-    def position(self):
-        return(self.__position)
 
     @size.setter
     # This sets the size
@@ -37,9 +34,15 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    # This gets the position
+    def position(self):
+        return(self.__position)
+
+    @position.setter
     # This sets the position
     def position(self, value):
-        if not value[0] or not value[1]:
+        if value[0] is None or value[1] is None:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -53,12 +56,12 @@ class Square:
 
     # Public, prints square
     def my_print(self):
-        set = self.__position[:]
-        x = set[0]
-        y = set[1]
-        print("{}".format('\n' * y), end='')
         if self.__size == 0:
             print()
         else:
+            set = self.__position[:]
+            x = set[0]
+            y = set[1]
+            print("{}".format('\n' * y), end='')
             for i in range(0, self.__size):
                 print("{}{}".format(' ' * x, '#' * self.__size))
