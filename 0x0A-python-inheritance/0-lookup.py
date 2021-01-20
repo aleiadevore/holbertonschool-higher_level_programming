99e7a496a89f7ec566a751e115d84e40af212d3c
Aleia DeVore <2496@holbertonschool.com>
2021-01-20 01:16:59 +0000

    Created function to return attributes and methods of object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This function returns a list of available methods and attributes
+of an object"""
+
+
+def lookup(obj):
+    """Returns list of methods and attributes of obj"""
+    return dir(obj)
