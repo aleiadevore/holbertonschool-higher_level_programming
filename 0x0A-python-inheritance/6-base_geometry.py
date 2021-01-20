ea3e29e9332ecd35feecf171d50ff2b5bf2a8c5c
Aleia DeVore <2496@holbertonschool.com>
2021-01-20 02:32:49 +0000

    Raised exception in class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates an empty class"""
+
+
+class BaseGeometry:
+    """This class is empty"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
