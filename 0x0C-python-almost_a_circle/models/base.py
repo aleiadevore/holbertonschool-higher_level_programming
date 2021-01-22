a4919bbfdacbc274960686632174f196b5474e6a
Aleia DeVore <2496@holbertonschool.com>
2021-01-22 20:32:31 +0000

    Created base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a base class"""
+
+
+class Base:
+    """This is the base class for the project"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
