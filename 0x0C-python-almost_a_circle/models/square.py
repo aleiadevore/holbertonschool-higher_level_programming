f12cb26cfbad7a7aa319c570a1ac19e54972206f
Aleia DeVore <2496@holbertonschool.com>
2021-01-27 16:28:14 +0000

    Fixed init issue

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ This initializes the square """
 
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ This returns the string representation """
