cd1311870ed211572cf23515bd67fed5581ee1f4
Aleia DeVore <2496@holbertonschool.com>
2021-03-02 02:33:59 +0000

    Fixed Alex's score

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ score INT);
 INSERT INTO second_table
 VALUES (1, 'John', 10);
 INSERT INTO second_table
-VALUES (2, 'Alex', 2);
+VALUES (2, 'Alex', 3);
 INSERT INTO second_table
 VALUES (3, 'Bob', 14);
 INSERT INTO second_table
