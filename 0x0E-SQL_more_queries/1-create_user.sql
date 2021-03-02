f428ef5e4ea1c555b95b931d6a981c8c9a1c1eee
Aleia DeVore <2496@holbertonschool.com>
2021-03-02 22:04:17 +0000

    Creates new user and sets password

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates user with all priviledges and sets password
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
