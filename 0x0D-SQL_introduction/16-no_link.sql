8494528c8d482ffddf19d8dc9e31477a5ced3105
Aleia DeVore <2496@holbertonschool.com>
2021-03-02 03:22:28 +0000

    Fixed capitalization

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Lists all records of second_table with name
 -- Displays score and name by high score
-SELECT score, name from second_table
+SELECT score, name FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC
