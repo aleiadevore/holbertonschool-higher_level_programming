788e238499585562da60026821e50eaf3e6b52f4
Aleia DeVore <2496@holbertonschool.com>
2021-03-22 22:11:59 +0000

    Found second largest input number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+let large = 0;
+let ans = 0;
+let i = 2;
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  large = process.argv[2];
+  for (; process.argv[i] != null; i++) {
+    if (large >= ans) {
+      ans = large;
+      large = process.argv[i];
+    }
+  } console.log(ans);
+}
