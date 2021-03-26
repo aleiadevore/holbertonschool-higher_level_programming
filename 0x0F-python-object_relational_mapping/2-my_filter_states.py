3c687d65fcc2cd6dad4b0cabc6823e8e7e741d1c
Aleia DeVore <2496@holbertonschool.com>
2021-03-26 15:38:16 +0000

    Takes user input for command

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This module selects all states beginning with 'N' """
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                     host="localhost", port=3306)
+c = db.cursor()
+cmd = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(
+        sys.argv[4])
+c.execute(cmd)
+q_rows = c.fetchall()
+for row in q_rows:
+        print(row)
+c.close()
+db.close()
