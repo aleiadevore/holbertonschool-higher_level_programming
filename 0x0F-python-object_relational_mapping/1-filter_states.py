ae9d2f52e1ce5a29777ae675c3bae13e6a66d326
Aleia DeVore <2496@holbertonschool.com>
2021-03-26 15:15:11 +0000

    Filtered by name

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module selects all states beginning with 'N' """
+
+import MySQLdb
+import sys
+
+
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                     host="localhost", port=3306)
+c = db.cursor()
+c.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
+q_rows = c.fetchall()
+for row in q_rows:
+        print(row)
+c.close()
+db.close()
