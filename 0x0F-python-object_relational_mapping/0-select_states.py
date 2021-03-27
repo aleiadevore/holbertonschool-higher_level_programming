000dbb0a75ee76db449a13177ba425b0b0ac3227
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 17:50:20 +0000

    Fixed not executing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,13 @@
 import MySQLdb
 import sys
 
-
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                     host="localhost", port=3306)
-c = db.cursor()
-c.execute("""SELECT * FROM states ORDER BY id ASC""")
-q_rows = c.fetchall()
-for row in q_rows:
-        print(row)
-c.close()
-db.close()
+if __name__ == "__main__":
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                             db=sys.argv[3], host="localhost", port=3306)
+        c = db.cursor()
+        c.execute("""SELECT * FROM states ORDER BY id ASC""")
+        q_rows = c.fetchall()
+        for row in q_rows:
+                print(row)
+        c.close()
+        db.close()
