e2ea6ab0640a36289168a4ab2e21bfb522a24e9e
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 02:31:28 +0000

    Autoincriment for id not working, saving progress

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This model creates a States class """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from uuid import uuid4
+
+Base = declarative_base()
+
+class State(Base):
+        """ This creates class from table states """
+
+        __tablename__ = "states"
+        """ id is class attribute that represents a column of an auto-generated, unique integer, can’t be null and is a primary key """
+        id = Column(Integer, autoincrement=True, default=uuid4, nullable=False, primary_key=True)
+
+        """ name is a class attribute that represents a column of a string with maximum 128 characters and can’t be null """
+        name = Column(String(128), nullable=False)
