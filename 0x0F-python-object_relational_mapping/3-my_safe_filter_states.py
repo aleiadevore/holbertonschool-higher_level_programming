a690286126760a86b11d125b7f640692c5c48539
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 18:02:08 +0000

    Fixed requirements

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,21 +4,22 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                     host="localhost", port=3306)
-c = db.cursor()
-inpt = sys.argv[4]
-newinpt = ""
-for i in range(len(inpt)):
-        if i != 0 and (inpt[i] == '\'' or inpt[i] == '\"'):
-                break
-        else:
-                newinpt += inpt[i]
-cmd = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(
-        newinpt)
-c.execute(cmd)
-q_rows = c.fetchall()
-for row in q_rows:
-        print(row)
-c.close()
-db.close()
+if __name__ == "__main__":
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                             db=sys.argv[3], host="localhost", port=3306)
+        c = db.cursor()
+        inpt = sys.argv[4]
+        newinpt = ""
+        for i in range(len(inpt)):
+                if i != 0 and (inpt[i] == '\'' or inpt[i] == '\"'):
+                        break
+                else:
+                        newinpt += inpt[i]
+        cmd = "SELECT * FROM states WHERE name LIKE '{}'\
+                 ORDER BY id ASC".format(newinpt)
+        c.execute(cmd)
+        q_rows = c.fetchall()
+        for row in q_rows:
+                print(row)
+        c.close()
+        db.close()
