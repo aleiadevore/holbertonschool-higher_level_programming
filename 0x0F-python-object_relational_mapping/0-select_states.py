4b839a2bcfc254d7f559bd06ba44359de8beb96e
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 17:32:23 +0000

    updating formatting

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ This module lists all states from the database hbtn_0e_0_usa """
-
 import MySQLdb
 import sys
+
 
 db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
                      host="localhost", port=3306)
