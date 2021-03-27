45caca7192d00b10e4377f76eb7fb34378864e6a
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 18:19:04 +0000

    Fixing requirements

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,11 +9,11 @@ from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-match = session.query(State).order_by(State.id).first()
-print("{}: {}".format(match.id, match.name))
+    match = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(match.id, match.name))
