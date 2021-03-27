77e75425205f58e81acb65d9cba0689968851b17
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 02:35:09 +0000

    Removed uuid

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,6 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from uuid import uuid4
 
 Base = declarative_base()
 
@@ -12,7 +11,7 @@ class State(Base):
 
         __tablename__ = "states"
         """ id is class attribute that represents a column of an auto-generated, unique integer, can’t be null and is a primary key """
-        id = Column(Integer, autoincrement=True, default=uuid4, nullable=False, primary_key=True)
+        id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
 
         """ name is a class attribute that represents a column of a string with maximum 128 characters and can’t be null """
         name = Column(String(128), nullable=False)
