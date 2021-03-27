c567b753c11afd4ca44224b1012b9fbd2300fc24
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 18:28:30 +0000

    Fixed requirements

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,26 +8,26 @@ from sqlalchemy import (create_engine)
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        Base.metadata.create_all(engine)
 
-""" Filtering user input (state) to prevent injection """
-inpt = sys.argv[4]
-newinpt = ""
-for i in range(len(inpt)):
-        if i != 0 and (inpt[i] == '\'' or inpt[i] == '\"'):
-                break
+        """ Filtering user input (state) to prevent injection """
+        inpt = sys.argv[4]
+        newinpt = ""
+        for i in range(len(inpt)):
+                if i != 0 and (inpt[i] == '\'' or inpt[i] == '\"'):
+                        break
+                else:
+                        newinpt += inpt[i]
+
+        """ Filtering and printing """
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        match = session.query(State).filter(State.name.like
+                                            (inpt)).first()
+        if (match is not None):
+                print(match.id)
         else:
-                newinpt += inpt[i]
-
-""" Filtering and printing """
-Session = sessionmaker(bind=engine)
-session = Session()
-
-match = session.query(State).filter(State.name.like
-                                    (inpt)).first()
-if (match is not None):
-    print(match.id)
-else:
-    print("Not found")
+                print("Not found")
