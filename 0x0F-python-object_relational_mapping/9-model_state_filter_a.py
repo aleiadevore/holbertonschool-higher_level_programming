0d04d6a5f4c46d324e0b971f92340f4a1ec81933
Aleia DeVore <2496@holbertonschool.com>
2021-03-27 18:26:19 +0000

    Fixed requirements

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-for instance in session.query(State).filter(State.name.like
-                                            ('%a%')).order_by(State.id):
-        print("{}: {}".format(instance.id, instance.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter(State.name.like
+                                                ('%a%')).order_by(State.id):
+            print("{}: {}".format(instance.id, instance.name))
