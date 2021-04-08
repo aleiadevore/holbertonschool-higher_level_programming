fa02e1c8b07fef9db0eaecbdb3752bddea84b2aa
Aleia DeVore <2496@holbertonschool.com>
2021-04-08 21:39:33 +0000

    Remembered to take user input

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends post parameters
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST 0.0.0.0:5000/route_6
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
