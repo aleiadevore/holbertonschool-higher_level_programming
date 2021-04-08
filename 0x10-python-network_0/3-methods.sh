974de3b71a0c8efe8377b022b60e372568d56d27
Aleia DeVore <2496@holbertonschool.com>
2021-04-08 21:24:05 +0000

    Used options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This won't work
-curl -s -i "$1" | grep "Allow:" | awk '{ print substr($0, index($0,$2)) }'
+curl -siX OPTIONS "$1" | grep "Allow:" | awk '{ print substr($0, index($0,$2)) }'
