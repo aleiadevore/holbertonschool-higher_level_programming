69f776680e481ed5a4a1b7ea0d65ba8a365aebeb
Aleia DeVore <2496@holbertonschool.com>
2021-04-08 21:40:35 +0000

    Added s flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends post parameters
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -sX POST "$1"
