773daada66cc4d72fb043075bbf50b6232836388
Aleia DeVore <2496@holbertonschool.com>
2021-04-09 19:03:06 +0000

    Headers (info) has items to be accessed as dict

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,9 +7,7 @@ from urllib import request
 
 
 if __name__ == "__main__":
-        #data = "X-Request-Id".encode('utf-8')
-        #req = request.Request(argv[1], data=data)
         with request.urlopen(argv[1]) as text:
-                html = text.info()
-                for line in html:
-                        print(line)
+                for key, value in text.info().items():
+                        if key == "X-Request-Id":
+                                print(value)
