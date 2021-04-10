07d82c7f4375df597239c81dded6c9be5873d3b0
Aleia DeVore <2496@holbertonschool.com>
2021-04-10 04:05:01 +0000

    There is no pattern to which peak to return??

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!usr/bin/python3
+""" Finds peak (larger than either side) in list """
+
+
+def find_peak(list_of_integers):
+        """ Finds peak in list """
+        l = len(list_of_integers)
+        ans = 0
+
+        if l == 0:
+                return None
+        if l == 0:
+                return list_of_integers[0]
+
+        for i in range(1, l - 1):
+                if list_of_integers[i] >= list_of_integers[i - 1] \
+                        and list_of_integers[i] >= list_of_integers[i + 1]:
+                        ans = list_of_integers[i]
+
+        if ans == 0:
+                if list_of_integers[0] >= list_of_integers[1]:
+                        return list_of_integers[0]
+                if list_of_integers[l - 1] >= list_of_integers[l - 2]:
+                        return list_of_integers[l - 1]
+        else:
+                return ans
