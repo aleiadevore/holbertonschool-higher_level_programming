9b9feaa8ffb2e9b7afedf453fa6b4baa5c07600f
Aleia DeVore <2496@holbertonschool.com>
2021-05-07 00:20:31 +0000

    Doesn't use input

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// syntax: https://swapi-api.hbtn.io/api/films/{user input}, get title
+
+//const episode = process.argv[2];
+//const url = 'https://swapi-api.hbtn.io/api/films/'.concat(episode);
+const url = 'https://swapi-api.hbtn.io/api/people/18'
+request(url, function (error, response, body) {
+  if (error) throw error;
+  const movies = JSON.parse(body).films
+  console.log(movies.length);
+});
