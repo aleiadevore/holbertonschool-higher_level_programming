d036960f772445c5ab6dd5c08afc34e3579a5e96
Aleia DeVore <2496@holbertonschool.com>
2021-05-07 00:23:53 +0000

    Semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,11 @@ const request = require('request');
 
 // syntax: https://swapi-api.hbtn.io/api/films/{user input}, get title
 
-//const episode = process.argv[2];
-//const url = 'https://swapi-api.hbtn.io/api/films/'.concat(episode);
-const url = 'https://swapi-api.hbtn.io/api/people/18'
+// const episode = process.argv[2];
+// const url = 'https://swapi-api.hbtn.io/api/films/'.concat(episode);
+const url = 'https://swapi-api.hbtn.io/api/people/18';
 request(url, function (error, response, body) {
   if (error) throw error;
-  const movies = JSON.parse(body).films
+  const movies = JSON.parse(body).films;
   console.log(movies.length);
 });
