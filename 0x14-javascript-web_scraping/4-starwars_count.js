0f7e59a85d1f8803e4dc56408f400503dbd3e774
Aleia DeVore <2496@holbertonschool.com>
2021-05-07 00:52:26 +0000

    Switched to counting for every movie despite there being a tool specifically for this in the api

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,19 @@ const request = require('request');
 
 // syntax: https://swapi-api.hbtn.io/api/films/{user input}, get title
 
-// const episode = process.argv[2];
-// const url = 'https://swapi-api.hbtn.io/api/films/'.concat(episode);
-const url = 'https://swapi-api.hbtn.io/api/people/18';
+const url = process.argv[2];
+let count = 0;
+
+// const url = 'https://swapi-api.hbtn.io/api/people/18';
 request(url, function (error, response, body) {
   if (error) throw error;
-  const movies = JSON.parse(body).films;
-  console.log(movies.length);
+  const movies = JSON.parse(body).results;
+  for (const i in movies) {
+    for (const char in movies[i].characters) {
+      if (movies[i].characters[char].includes('18')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
